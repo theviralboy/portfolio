89553e3cc34e7949ace20ed57e2ff6e1eae43a60
Sahil Verma <studentsahil999@gmail.com>
2021-10-27 10:44:04 +0530

    Pages updates

--- src/Pages/Projects/index.jsx
+++ src/Pages/Projects/index.jsx
@@ -1,21 +1,54 @@
-import React from "react";
-import Projects from "../../Components/Projects";
+import React, { useState } from "react";
 
-// importing framer motion
-import { motion } from "framer-motion";
+// importing react router dom
+import { Link } from "react-router-dom";
+
+// importing components
+import ProjectCard from "../../Components/Projects/ProjectCard";
+
+// importing fireabse
+import { firestore } from "../../Firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 
 const SingleProjects = () => {
+  const [projects, setProjects] = useState([]);
+  const dataRef = collection(firestore, "projects");
+  getDocs(dataRef)
+    .then((data) => {
+      setProjects(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    })
+    .catch((err) => console.log(err));
   return (
-    <motion.div
-      initial={{ scaleY: 0 }}
-      animate={{ scaleY: 1 }}
-      exit={{ scaleY: 0 }}
-      transition={{ duration: 0.5 }}
+    <section
+      className="projects"
+      style={{
+        marginTop: "30px",
+      }}
     >
-      <div className="full-page">
-        <Projects />
+      <div className="projects-inner">
+        <div className="container">
+          <div className="row">
+            {projects?.map((project) => {
+              return (
+                <div
+                  className="col-lg-4 col-md-6 col-sm-6 col-12"
+                  key={project.Id}
+                >
+                  <Link to={`/project/${project.Id}`}>
+                    <ProjectCard
+                      image={project.Image}
+                      name={project.Name}
+                      logo={project.Logo}
+                      tag={project.Stack[0]}
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </motion.div>
+    </section>
   );
 };
 
--- src/Pages/Skills/index.jsx
+++ src/Pages/Skills/index.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Skills from "../../Components/Skills";
 
+import { SkillCard } from "../../Components/Skills";
+import SkillData from "../../Components/Skills/SkillsData";
+
 const SingleSkills = () => {
   return (
-    <div className="full-page">
-      <Skills />
-    </div>
+    <section
+      className="full-page"
+      style={{
+        marginTop: "50px",
+      }}
+    >
+      <div className="container">
+        <div className="row">
+          {SkillData.map((skill) => {
+            return (
+              <div className=" col-md-4 col-6 p-0" key={skill.id}>
+                <SkillCard name={skill.name} icon={skill.icon} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 };
 
